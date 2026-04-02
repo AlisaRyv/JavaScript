4b79f6537c518c244613a1f4be37d8faf50a447e
AlisaRyv <aliceryvkina2@gmail.com>
2026-04-02 11:20:24 +0300

    Finish Hw_3.html & Task_3.js

--- Homeworks/Homework_1/Homework_1_3/Task_3.js
+++ Homeworks/Homework_1/Homework_1_3/Task_3.js
@@ -1,5 +1,6 @@
-let firstName = `Аліса`;
-let middleName = `Віталіївна`;
-let lastName = `Ривкіна`;
-let person = lastName+` `+firstName+` `+middleName;
-console.log(person);
+let a = 100;
+let b = `100`;
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
