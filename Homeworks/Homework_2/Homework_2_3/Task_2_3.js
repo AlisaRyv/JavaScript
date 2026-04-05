84be40222a68651e2b147fcb9f0c005545fb0062
AlisaRyv <aliceryvkina2@gmail.com>
2026-04-05 14:56:52 +0300

    Start Hw_2_3.html & Task_2_3.js

---
+++ Homeworks/Homework_2/Homework_2_3/Task_2_3.js
@@ -0,0 +1,15 @@
+let obj1= {
+    title: `Книга 1`,
+    pageCount: 400,
+    genre: `Жанр 1`
+};
+let obj2= {
+    title: `Книга 2`,
+    pageCount: 500,
+    genre: `Жанр 2`
+};
+let obj3= {
+    title: `Книга 3`,
+    pageCount: 300,
+    genre: `Жанр 3`
+};
