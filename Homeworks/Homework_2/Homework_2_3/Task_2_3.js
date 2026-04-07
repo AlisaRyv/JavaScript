883eaf44cc7d22c5065fb553910f544eca13fcd4
AlisaRyv <aliceryvkina2@gmail.com>
2026-04-07 11:43:50 +0300

    Finish Hw_2_3.html & Task_2_3.js

--- Homeworks/Homework_2/Homework_2_3/Task_2_3.js
+++ Homeworks/Homework_2/Homework_2_3/Task_2_3.js
@@ -1,15 +1,18 @@
 let obj1= {
     title: `Книга 1`,
     pageCount: 400,
-    genre: `Жанр 1`
+    genre: `Жанр 1`,
+    authors: [{name:`Борис`, age: 23}, {name:`Александра`, age: 41}]
 };
 let obj2= {
     title: `Книга 2`,
     pageCount: 500,
-    genre: `Жанр 2`
+    genre: `Жанр 2`,
+    authors: [{name:`Григорий`, age: 3}, {name:`Антон`, age: 19}, {name:`Ольга`, age: 37}]
 };
 let obj3= {
     title: `Книга 3`,
     pageCount: 300,
-    genre: `Жанр 3`
+    genre: `Жанр 3`,
+    authors: [{name:`Аркадий`, age: 53}, {name:`Вольф`, age: 33}, {name:`Алиса`, age: 20}, {name:`Прасковья`, age: 60}]
 };
