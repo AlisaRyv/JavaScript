017e76ae4f0d07d9d5e13f3ec5d5aac929601e33
AlisaRyv <aliceryvkina2@gmail.com>
2026-04-08 11:25:25 +0300

    Finish Hw_2_6.html & Task_2_6.js

--- Homeworks/Homework_2/Homework_2_6/Task_2_6.js
+++ Homeworks/Homework_2/Homework_2_6/Task_2_6.js
@@ -1,9 +1,9 @@
-let arr=[
-    {day:`first-day`, morning_temperature: 25, afternoon_temperature: 35, night_temperature: 20},
-    {day:`second-day`, morning_temperature: 5, afternoon_temperature: 17, night_temperature: 10},
-    {day:`third-day`, morning_temperature: -2, afternoon_temperature: 2, night_temperature: 0},
-    {day:`fourth-day`, morning_temperature: 32, afternoon_temperature: 45, night_temperature: 23},
-    {day:`fifth-day`, morning_temperature: 0, afternoon_temperature: 5, night_temperature: 10},
-    {day:`sixth-day`, morning_temperature: -10, afternoon_temperature: -5, night_temperature: -20},
-    {day:`seventh-day`, morning_temperature: 7, afternoon_temperature: 18, night_temperature: 9}
-]
+let x=prompt(`Для перевірки введіть: 1, 0, -3`);
+if(x!==`0`)
+{
+    console.log(`Вірно`);
+}
+else
+{
+    console.log(`Невірно`);
+}
