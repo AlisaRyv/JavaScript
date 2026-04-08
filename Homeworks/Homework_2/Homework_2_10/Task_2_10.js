fface69a0c41f63a343548c5f98ead3ba87bb135
AlisaRyv <aliceryvkina2@gmail.com>
2026-04-08 14:31:49 +0300

    Finish Hw_2_10.html & Task_2_10.js

--- Homeworks/Homework_2/Homework_2_10/Task_2_10.js
+++ Homeworks/Homework_2/Homework_2_10/Task_2_10.js
@@ -1,49 +1,14 @@
-let day=+prompt(`Введіть порядковий номер дня тижня: `);
-switch (day)
+let x=+prompt(`Enter first number:`);
+let y=+prompt(`Enter second number:`);
+if(x>y)
 {
-    case 1:
-        console.log(`123`);
-        console.log(`123`);
-        console.log(`123`);
-        console.log(`123`);
-        break;
-    case 2:
-        console.log(`abc`);
-        console.log(`abc`);
-        console.log(`abc`);
-        console.log(`abc`);
-        break;
-    case 3:
-        console.log(`321`);
-        console.log(`321`);
-        console.log(`321`);
-        console.log(`321`);
-        break;
-    case 4:
-        console.log(`cba`);
-        console.log(`cba`);
-        console.log(`cba`);
-        console.log(`cba`);
-        break;
-    case 5:
-        console.log(`456`);
-        console.log(`456`);
-        console.log(`456`);
-        console.log(`456`);
-        break;
-    case 6:
-        console.log(`654`);
-        console.log(`654`);
-        console.log(`654`);
-        console.log(`654`);
-        break;
-    case 7:
-        console.log(`lms`);
-        console.log(`lms`);
-        console.log(`lms`);
-        console.log(`lms`);
-        break;
-    default:
-        console.log(`Введіть коректне число (від 1 до 7)!`)
-        break;
+    console.log(`Max number: x = `+x);
+} else if(y<x)
+{
+    console.log(`Max number: y = `+y);
+} else if(y===x)
+{
+    console.log(`It's the same numbers: x = y = `+x);
+} else{
+    console.log(`ERROR! Enter the correct numbers.`);
 }
