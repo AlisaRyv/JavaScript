53186667aae0cac04cad0d7e543e1e55e5753747
AlisaRyv <aliceryvkina2@gmail.com>
2026-04-08 11:10:41 +0300

    Finish Hw_2_5.html & Task_2_5.js

--- Homeworks/Homework_2/Homework_2_5/Task_2_5.js
+++ Homeworks/Homework_2/Homework_2_5/Task_2_5.js
@@ -1,22 +1,9 @@
 let arr=[
-    {name:`user1`, username: `krokodile`, password: `123456789@`},
-    {name:`user2`, username: `krok`, password: `12345678@`},
-    {name:`user3`, username: `dile`, password: `1234567@`},
-    {name:`user4`, username: `kod`, password: `123456@`},
-    {name:`user5`, username: `kro`, password: `12345@`},
-    {name:`user6`, username: `koile`, password: `1234@`},
-    {name:`user7`, username: `krole`, password: `123@`},
-    {name:`user8`, username: `krokod`, password: `12@`},
-    {name:`user9`, username: `ile`, password: `1@`},
-    {name:`user10`, username: `oko`, password: `0@`},
-];
-console.log(arr[0].password);
-console.log(arr[1].password);
-console.log(arr[2].password);
-console.log(arr[3].password);
-console.log(arr[4].password);
-console.log(arr[5].password);
-console.log(arr[6].password);
-console.log(arr[7].password);
-console.log(arr[8].password);
-console.log(arr[9].password);
+    {day:`first-day`, morning_temperature: 25, afternoon_temperature: 35, night_temperature: 20},
+    {day:`second-day`, morning_temperature: 5, afternoon_temperature: 17, night_temperature: 10},
+    {day:`third-day`, morning_temperature: -2, afternoon_temperature: 2, night_temperature: 0},
+    {day:`fourth-day`, morning_temperature: 32, afternoon_temperature: 45, night_temperature: 23},
+    {day:`fifth-day`, morning_temperature: 0, afternoon_temperature: 5, night_temperature: 10},
+    {day:`sixth-day`, morning_temperature: -10, afternoon_temperature: -5, night_temperature: -20},
+    {day:`seventh-day`, morning_temperature: 7, afternoon_temperature: 18, night_temperature: 9}
+]
