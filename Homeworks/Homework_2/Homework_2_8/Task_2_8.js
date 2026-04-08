3d2df5a1ef09f00ce28e21e764bcbea25585ddf7
AlisaRyv <aliceryvkina2@gmail.com>
2026-04-08 11:46:02 +0300

    Start Hw_2_8.html & Task_2_8.js

---
+++ Homeworks/Homework_2/Homework_2_8/Task_2_8.js
@@ -0,0 +1,19 @@
+let time=+prompt(`Введіть хвилини (від 0 до 59):`);
+if(time >= 0 && time < 15)
+{
+    console.log(`1 чверть`);
+}
+else if(time >= 15 && time < 30)
+{
+    console.log(`2 чверть`);
+}else if(time >= 30 && time < 45)
+{
+    console.log(`3 чверть`);
+}else if(time >= 45 && time <= 59)
+{
+    console.log(`4 чверть`);
+}
+else
+{
+    console.log(`Помилка! Введено некоректний час.`);
+}
