aa82429f7853acb7c2cfa349eb8e83ba016e487e
AlisaRyv <aliceryvkina2@gmail.com>
2026-04-08 14:18:59 +0300

    Start Hw_2_10.html & Task_2_10.js

---
+++ Homeworks/Homework_2/Homework_2_10/Task_2_10.js
@@ -0,0 +1,49 @@
+let day=+prompt(`Введіть порядковий номер дня тижня: `);
+switch (day)
+{
+    case 1:
+        console.log(`123`);
+        console.log(`123`);
+        console.log(`123`);
+        console.log(`123`);
+        break;
+    case 2:
+        console.log(`abc`);
+        console.log(`abc`);
+        console.log(`abc`);
+        console.log(`abc`);
+        break;
+    case 3:
+        console.log(`321`);
+        console.log(`321`);
+        console.log(`321`);
+        console.log(`321`);
+        break;
+    case 4:
+        console.log(`cba`);
+        console.log(`cba`);
+        console.log(`cba`);
+        console.log(`cba`);
+        break;
+    case 5:
+        console.log(`456`);
+        console.log(`456`);
+        console.log(`456`);
+        console.log(`456`);
+        break;
+    case 6:
+        console.log(`654`);
+        console.log(`654`);
+        console.log(`654`);
+        console.log(`654`);
+        break;
+    case 7:
+        console.log(`lms`);
+        console.log(`lms`);
+        console.log(`lms`);
+        console.log(`lms`);
+        break;
+    default:
+        console.log(`Введіть коректне число (від 1 до 7)!`)
+        break;
+}
