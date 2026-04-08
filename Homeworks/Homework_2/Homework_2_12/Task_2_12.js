241ba363d8a28c93b8e2446073e72628ee93764a
AlisaRyv <aliceryvkina2@gmail.com>
2026-04-08 15:44:52 +0300

    Finish Hw_2_12.html & Task_2_12.js

--- Homeworks/Homework_2/Homework_2_12/Task_2_12.js
+++ Homeworks/Homework_2/Homework_2_12/Task_2_12.js
@@ -1,6 +1,38 @@
-let x=``;
-if(!x)
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+let counter= coursesAndDurationArray[0];
+if(counter.monthDuration>5)
 {
-    x=`default`;
+    console.log(counter.title+` SUPER`);
 }
-console.log(x);
+counter= coursesAndDurationArray[1];
+if(counter.monthDuration>5)
+{
+    console.log(counter.title+` SUPER`);
+}
+counter= coursesAndDurationArray[2];
+if(counter.monthDuration>5)
+{
+    console.log(counter.title+` SUPER`);
+}
+counter= coursesAndDurationArray[3];
+if(counter.monthDuration>5)
+{
+    console.log(counter.title+` SUPER`);
+}
+counter= coursesAndDurationArray[4];
+if(counter.monthDuration>5)
+{
+    console.log(counter.title+` SUPER`);
+}
+counter= coursesAndDurationArray[5];
+if(counter.monthDuration>5)
+{
+    console.log(counter.title+` SUPER`);
+}
