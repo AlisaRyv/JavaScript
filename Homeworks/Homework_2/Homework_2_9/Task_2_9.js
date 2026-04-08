a94fd662dc25ed747b4d91e601c122f9d631ac95
AlisaRyv <aliceryvkina2@gmail.com>
2026-04-08 14:17:49 +0300

    Finish Hw_2_9.html & Task_2_9.js

--- Homeworks/Homework_2/Homework_2_9/Task_2_9.js
+++ Homeworks/Homework_2/Homework_2_9/Task_2_9.js
@@ -1,15 +1,49 @@
-let day=+prompt(`Введіть день (від 1 до 31):`);
-if(day >= 1 && day <= 10)
+let day=+prompt(`Введіть порядковий номер дня тижня: `);
+switch (day)
 {
-    console.log(`1 декада`);
-}
-else if(day > 10 && day <= 20)
-{
-    console.log(`2 декада`);
-}else if(day > 20 && day <= 31)
-{
-    console.log(`3 декада`);
-} else
-{
-    console.log(`Помилка! Введено некоректний день.`);
+    case 1:
+        console.log(`123`);
+        console.log(`123`);
+        console.log(`123`);
+        console.log(`123`);
+        break;
+    case 2:
+        console.log(`abc`);
+        console.log(`abc`);
+        console.log(`abc`);
+        console.log(`abc`);
+        break;
+    case 3:
+        console.log(`321`);
+        console.log(`321`);
+        console.log(`321`);
+        console.log(`321`);
+        break;
+    case 4:
+        console.log(`cba`);
+        console.log(`cba`);
+        console.log(`cba`);
+        console.log(`cba`);
+        break;
+    case 5:
+        console.log(`456`);
+        console.log(`456`);
+        console.log(`456`);
+        console.log(`456`);
+        break;
+    case 6:
+        console.log(`654`);
+        console.log(`654`);
+        console.log(`654`);
+        console.log(`654`);
+        break;
+    case 7:
+        console.log(`lms`);
+        console.log(`lms`);
+        console.log(`lms`);
+        console.log(`lms`);
+        break;
+    default:
+        console.log(`Введіть коректне число (від 1 до 7)!`)
+        break;
 }
