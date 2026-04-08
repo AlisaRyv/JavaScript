3888c662b4166ab2bac1b4d9cc72e8707ea32632
AlisaRyv <aliceryvkina2@gmail.com>
2026-04-08 15:26:15 +0300

    Finish Hw_2_11.html & Task_2_11.js

--- Homeworks/Homework_2/Homework_2_11/Task_2_11.js
+++ Homeworks/Homework_2/Homework_2_11/Task_2_11.js
@@ -1,14 +1,6 @@
-let x=+prompt(`Enter first number:`);
-let y=+prompt(`Enter second number:`);
-if(x>y)
+let x=``;
+if(!x)
 {
-    console.log(`Max number: x = `+x);
-} else if(y<x)
-{
-    console.log(`Max number: y = `+y);
-} else if(y===x)
-{
-    console.log(`It's the same numbers: x = y = `+x);
-} else{
-    console.log(`ERROR! Enter the correct numbers.`);
+    x=`default`;
 }
+console.log(x);
