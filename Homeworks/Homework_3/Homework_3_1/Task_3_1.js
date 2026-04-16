68ea6f7901c5124a35ea6670de4a48a90c303ae7
AlisaRyv <aliceryvkina2@gmail.com>
2026-04-16 12:03:38 +0300

    Finish Hw_3_1.html & Task_3_1.js

---
+++ Homeworks/Homework_3/Homework_3_1/Task_3_1.js
@@ -0,0 +1,5 @@
+for(let i=0; i<10;i++){
+    document.write(`<div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, eligendi est facilis iure magni modi molestias nihil praesentium sed ut!
+    </div>`);
+}
