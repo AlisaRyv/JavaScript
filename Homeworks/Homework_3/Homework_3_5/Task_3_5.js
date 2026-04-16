fa53da18db46d1bb025f3f7003542908dc37f125
AlisaRyv <aliceryvkina2@gmail.com>
2026-04-16 12:43:58 +0300

    Finish Hw_3_5.html & Task_3_5.js

--- Homeworks/Homework_3/Homework_3_5/Task_3_5.js
+++ Homeworks/Homework_3/Homework_3_5/Task_3_5.js
@@ -1,6 +1,10 @@
-let i =0;
-while(i<20)
-{
-    document.write(`<h1>Hello! ${i+1}</h1>`)
-    i++;
+let listOfItems = [`html`, `css`, `javascript`, `mysql`, `mongodb`, `react`, `angular`, `node.js`];
+
+document.write(`<ul>`);
+for (const item of listOfItems) {
+    document.write
+    (
+        `<li>${item}</li>`
+    );
 }
+document.write(`</ul>`);
