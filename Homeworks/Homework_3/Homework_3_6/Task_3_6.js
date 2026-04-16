ef54c9e1b77b85d53cbd111e96710488adbdb252
AlisaRyv <aliceryvkina2@gmail.com>
2026-04-16 13:20:24 +0300

    Finish Hw_3_6.html & Task_3_6.js

--- Homeworks/Homework_3/Homework_3_6/Task_3_6.js
+++ Homeworks/Homework_3/Homework_3_6/Task_3_6.js
@@ -1,10 +1,36 @@
-let listOfItems = [`html`, `css`, `javascript`, `mysql`, `mongodb`, `react`, `angular`, `node.js`];
+let products = [
 
-document.write(`<ul>`);
-for (const item of listOfItems) {
+    {
+        title: `milk`,
+        price: 22,
+        image: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOJDosJVsQj0P-kykfa-aGNX54PI-DMIhYEQ&s`
+    },
+
+    {
+        title: `juice`,
+        price: 27,
+        image: `https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg`
+    },
+
+    {
+        title: `tomato`,
+        price: 47,
+        image: `https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74`
+    },
+
+    {
+        title: `tea`,
+        price: 15,
+        image: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRaFj1YEmqAwSCneZpV-Ltw2Bo1l8EK4Ms6Q&s`
+    },
+
+];
+for (const product of products) {
     document.write
-    (
-        `<li>${item}</li>`
-    );
+    (`  
+   <div class="product-card">
+   <h3 class="product-title">${product.title}. Price – ${product.price}</h3>
+   <img src="${product.image}" alt="" class="product-image">
+   </div>
+    `)
 }
-document.write(`</ul>`);
