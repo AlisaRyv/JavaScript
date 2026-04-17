63877f4949bffe508b07bd9605bf5c732e0aafe7
AlisaRyv <aliceryvkina2@gmail.com>
2026-04-17 16:59:44 +0300

    Finish Add_Task_3.js & Hw_add_3.html

--- Homeworks/Homework_3/Additional_Homework/Add_Homework_3/Add_Task_3.js
+++ Homeworks/Homework_3/Additional_Homework/Add_Homework_3/Add_Task_3.js
@@ -1,5 +1,98 @@
-let arr=[];
-for(let i=0;i<10;i++){
-    arr[i]=(i+1)*2;
-}
-console.log(arr);
+let arr=[2,17,13,6,22,31,45,66,100,-18];
+let i=0;
+
+// 1 TASK
+// while(i<arr.length){
+//     console.log(arr[i]);
+//     i++;
+// }
+
+// 2 TASK
+// for(i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+// 3 TASK
+// while(i<arr.length){
+//     if(i%2!==0)
+//     console.log(arr[i]);
+//     i++;
+// }
+
+// 4 TASK
+// for(let i=0;i<arr.length;i++){
+//     if(i%2!==0)
+//     console.log(arr[i]);
+// }
+
+// 5 TASK
+// while(i<arr.length){
+//     if(arr[i]%2===0)
+//     console.log(arr[i]);
+//     i++;
+// }
+
+// 6 TASK
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]%2===0)
+//     console.log(arr[i]);
+// }
+
+// 7 TASK
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]%3===0)
+//         arr[i]=`okten`;
+// }
+// console.log(arr);
+
+// 8 TASK
+// for(let i=arr.length-1;i>=0;i--){
+//     console.log(arr[i]);
+// }
+
+// TASK 7 (REVERSE)
+// for(let i=arr.length-1;i>=0;i--){
+//     if(arr[i]%3===0)
+//         arr[i]=`okten`;
+//     console.log(arr[i]);
+// }
+
+// TASK 6 (REVERSE)
+// for(let i=arr.length-1;i>=0;i--){
+//     if(arr[i]%2===0)
+//     console.log(arr[i]);
+// }
+
+// TASK 5 (REVERSE)
+// i=arr.length-1;
+// while(i>=0){
+//     if(arr[i]%2===0)
+//     console.log(arr[i]);
+//     i--;
+// }
+
+// TASK 4 (REVERSE)
+// for(let i=arr.length-1;i>=0;i--){
+//     if(i%2!==0)
+//     console.log(arr[i]);
+// }
+
+// TASK 3 (REVERSE)
+// i=arr.length-1;
+// while(i>=0){
+//     if(i%2!==0)
+//     console.log(arr[i]);
+//     i--;
+// }
+
+// TASK 2 (REVERSE)
+// for(let i=arr.length-1;i>=0;i--){
+//     console.log(arr[i]);
+// }
+
+// TASK 1 (REVERSE)
+// i=arr.length-1;
+// while(i>=0){
+//     console.log(arr[i]);
+//     i--;
+// }
