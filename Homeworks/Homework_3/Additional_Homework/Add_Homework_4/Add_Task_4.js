f9ce0960a7733dbf7541ccd7910f5e09a8d6e00a
AlisaRyv <aliceryvkina2@gmail.com>
2026-04-18 20:01:51 +0300

    Finish Add_Task_4.js & Hw_add_4.html

---
+++ Homeworks/Homework_3/Additional_Homework/Add_Homework_4/Add_Task_4.js
@@ -0,0 +1,33 @@
+// #yHAwJOyiC
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// #GamKju89ob
+// let array = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`];
+
+// for (const arrayElement of array) {
+//     console.log(arrayElement);
+// }
+
+// Array for tasks: #Bm76xmg & #u3vmD0YJXh & #9stMq2ou & #mK4pmM4
+let array = [25, `b`, -9, `d`, `e`, true, `g`, `h`, false, 0];
+
+// #u3vmD0YJXh
+// for (const arrayElement of array) {
+//     if(typeof arrayElement === 'boolean') {
+//         console.log(arrayElement);
+//     }
+// }
+
+// #9stMq2ou
+// for (const arrayElement of array) {
+//     if(typeof arrayElement === 'number') {
+//         console.log(arrayElement);
+//     }
+// }
+
+// #mK4pmM4
+for (const arrayElement of array) {
+    if(typeof arrayElement === 'string') {
+        console.log(arrayElement);
+    }
+}
