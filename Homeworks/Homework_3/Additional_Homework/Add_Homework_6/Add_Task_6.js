d228c89a58a55f620887aac3b2a9397455d9a95d
AlisaRyv <aliceryvkina2@gmail.com>
2026-04-19 13:54:03 +0300

    Finish Add_Task_6.js & Hw_add_6.html

---
+++ Homeworks/Homework_3/Additional_Homework/Add_Homework_6/Add_Task_6.js
@@ -0,0 +1,39 @@
+// #mDMWMW5a
+// for (let i = 0; i < 10; i++)
+// {
+//     document.write(`<p>${i+1}</p>`);
+//     console.log(i+1);
+// }
+
+// #4sXhaa5YMM
+// for (let i = 0; i < 100; i++)
+// {
+//         document.write(`<p>${i+1}</p>`);
+//         console.log(i+1);
+// }
+
+// #s24slNyz7
+// for (let i = 0; i < 100; i+=2)
+// {
+//         document.write(`<p>${i+1}</p>`);
+//         console.log(i+1);
+// }
+
+// #zananT5FR1
+// for (let i = 1; i <= 100; i++)
+// {
+//     if(i%2===0)
+//     {
+//         document.write(`<p>${i}</p>`);
+//         console.log(i);
+//     }
+// }
+
+// #Tfrwls7FM
+// for (let i = 1; i <= 100; i++)
+// {
+//     if(i%2!==0) {
+//         document.write(`<p>${i}</p>`);
+//         console.log(i);
+//     }
+// }
