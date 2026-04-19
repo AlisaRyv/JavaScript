c0585ae82f0b04055a620a9bcf8bdb11f53d56af
AlisaRyv <aliceryvkina2@gmail.com>
2026-04-19 13:17:20 +0300

    Finish Add_Task_5.js & Hw_add_5.html

---
+++ Homeworks/Homework_3/Additional_Homework/Add_Homework_5/Add_Task_5.js
@@ -0,0 +1,14 @@
+let arr=[];
+arr[0]=1;
+arr[1]=`j`;
+arr[2]=true;
+arr[3]=undefined;
+arr[4]=-350;
+arr[5]=`abra`;
+arr[6]=false;
+arr[7]=``;
+arr[8]=0;
+arr[9]=`way`;
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
