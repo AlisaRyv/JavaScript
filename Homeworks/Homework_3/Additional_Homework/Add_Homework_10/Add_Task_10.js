9e5fb06dd9bc50ed5ea3e57fa027746d4ec52b9e
AlisaRyv <aliceryvkina2@gmail.com>
2026-04-20 12:35:10 +0300

    Finish Add_Task_10.js & Hw_add_10.html

---
+++ Homeworks/Homework_3/Additional_Homework/Add_Homework_10/Add_Task_10.js
@@ -0,0 +1,38 @@
+let usersWithId = [
+
+    {id: 1, name: `vasya`, age: 31, status: false},
+
+    {id: 2, name: `petya`, age: 30, status: true},
+
+    {id: 3, name: `kolya`, age: 29, status: true},
+
+    {id: 4, name: `olya`, age: 28, status: false}
+
+];
+
+let citiesWithId = [
+
+    {user_id: 3, country: `USA`, city: `Portland`},
+
+    {user_id: 1, country: `Ukraine`, city: `Ternopil`},
+
+    {user_id: 2, country: `Poland`, city: `Krakow`},
+
+    {user_id: 4, country: `USA`, city: `Miami`}
+
+];
+
+let usersWithCities=[];
+for (let i=0;i<usersWithId.length;i++)
+{
+    for (const cityWithId of citiesWithId)
+    {
+        if(usersWithId[i].id === cityWithId.user_id)
+        {
+            usersWithCities.push(usersWithId[i]);
+            usersWithCities[i].address=cityWithId;
+            break;
+        }
+    }
+}
+console.log(usersWithCities);
