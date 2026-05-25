f36f386977f1f7032e9a34883ae73bb1f3f92a71
AlisaRyv <aliceryvkina2@gmail.com>
2026-05-25 14:51:25 +0300

    Finish Hw_5_2.html & Task_5_2.js

--- Homeworks/Homework_5/Homework_5_2/Task_5_2.js
+++ Homeworks/Homework_5/Homework_5_2/Task_5_2.js
@@ -1,6 +1,7 @@
-function square_circle(r)
-{
-    return r*r*Math.PI;
-}
 let R=+prompt("Enter the Radius of Circle: ");
-console.log(square_circle(R));
+if(R>0) {
+    let square_circle = r => r*r*Math.PI;
+    console.log(square_circle(R));
+}
+else
+    throw new Error('Wrong Radius of Circle');
