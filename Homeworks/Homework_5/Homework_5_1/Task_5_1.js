5eeb6e1fa34ec3c944f9e2adde8b25916befee44
AlisaRyv <aliceryvkina2@gmail.com>
2026-05-25 14:47:51 +0300

    Finish Hw_5_1.html & Task_5_1.js

--- Homeworks/Homework_5/Homework_5_1/Task_5_1.js
+++ Homeworks/Homework_5/Homework_5_1/Task_5_1.js
@@ -1,7 +1,8 @@
-function square(a,b)
-{
-    return a*b;
-}
 let A=+prompt("Enter the Height of Rectangle: ");
 let B=+prompt("Enter the Width of Rectangle: ");
-console.log(square(A,B));
+if(A>0&&B>0) {
+    const square = (a, b) => a * b;
+    console.log(square(A, B));
+}
+else
+    throw new Error('Wrong Height/Width of Rectangle');
