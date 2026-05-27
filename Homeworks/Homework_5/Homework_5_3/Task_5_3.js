9b6f3e76e84c54744e3f8e1acfe6ea68b0a73c41
AlisaRyv <aliceryvkina2@gmail.com>
2026-05-27 17:35:54 +0300

    Finish Hw_5_1.html & Task_5_1.js

--- Homeworks/Homework_5/Homework_5_3/Task_5_3.js
+++ Homeworks/Homework_5/Homework_5_3/Task_5_3.js
@@ -1,11 +1,8 @@
-function square_circle(r,h)
-{
-    return 2*r*r*Math.PI+h*perimeter_circle(r);
-}
-function perimeter_circle(r)
-{
-    return 2*r*Math.PI;
-}
 let R=+prompt("Введите Радиус Цилиндра: ");
 let H=+prompt("Введите Высоту Цилиндра: ");
-console.log(square_circle(R,H));
+if(R>0&&H>0) {
+    let square_cilindre = (r,h) => (2*r*r*Math.PI+h*2*r*Math.PI);
+    console.log(square_cilindre(R,H));
+}
+else
+    throw new Error('Wrong Radius/Height of Cilindre');
