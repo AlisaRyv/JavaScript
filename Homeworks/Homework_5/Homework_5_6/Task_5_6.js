b57ed9d7e00797dce72fc6849b375ce820293dbd
AlisaRyv <aliceryvkina2@gmail.com>
2026-05-27 18:18:03 +0300

    Finish Hw_5_6.html & Task_5_6.js

--- Homeworks/Homework_5/Homework_5_6/Task_5_6.js
+++ Homeworks/Homework_5/Homework_5_6/Task_5_6.js
@@ -1,12 +1,3 @@
-function list(text, number)
-{
-    document.write(`<ul>`);
-    for (let i = 0; i < number; i++)
-    {
-        document.write(`<li>${text}</li>`);
-    }
-    document.write(`</ul>`);
-}
 let t=prompt("Enter a text: ");
-let n=+prompt("Enter a number: ");
-list(t,n);
+let list = text => (document.write(`<ul>`), document.write(`<li>${text}</li><li>${text}</li><li>${text}</li>`), document.write(`</ul>`));
+console.log(list(t));
