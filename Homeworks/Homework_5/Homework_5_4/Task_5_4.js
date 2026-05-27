2cd8058939e26f002fdd50c3936fdd58d6968fc8
AlisaRyv <aliceryvkina2@gmail.com>
2026-05-27 18:01:02 +0300

    Finish Hw_5_4.html & Task_5_4.js

--- Homeworks/Homework_5/Homework_5_4/Task_5_4.js
+++ Homeworks/Homework_5/Homework_5_4/Task_5_4.js
@@ -1,9 +1,2 @@
-function write(array)
-{
-    for(let i=0;i<array.length;i++)
-    {
-        console.log(array[i]);
-    }
-}
 let arr = [1,2,3,4,5,6,7,8,9,10];
-write(arr);
+arr.map(item=>console.log(item));
