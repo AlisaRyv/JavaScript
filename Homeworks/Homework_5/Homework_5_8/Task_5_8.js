60852680654f80ef62ba718c270e57afb70448ba
AlisaRyv <aliceryvkina2@gmail.com>
2026-05-29 12:02:48 +0300

    Finish Hw_5_8.html & Task_5_8.js

--- Homeworks/Homework_5/Homework_5_8/Task_5_8.js
+++ Homeworks/Homework_5/Homework_5_8/Task_5_8.js
@@ -1,11 +1,3 @@
-function list(array)
-{
-    document.write(`<ul>`);
-    for (let element of array)
-    {
-        document.write(`<li>${element}</li>`);
-    }
-    document.write(`</ul>`);
-}
 let arr=[`a`, 23, 90, true, `brak`, -35, false, 47, `b`, 5];
+let list=arr=>(document.write(`<ul>`),arr.map(item=>document.write(`<li>${item}</li>`)),   document.write(`</ul>`));
 list(arr);
