3613877a159b3df0b0571dae3d39916d947fd2c9
AlisaRyv <aliceryvkina2@gmail.com>
2026-05-29 21:05:13 +0300

    Finish Hw_5_13.html & Task_5_13.js

--- Homeworks/Homework_5/Homework_5_13/Task_5_13.js
+++ Homeworks/Homework_5/Homework_5_13/Task_5_13.js
@@ -1,13 +1,8 @@
-function exchange(sumUAH,currencyValues,exchangeCurrency)
-{
-    if(exchangeCurrency===currencyValues[0].currency)
-        return sumUAH/currencyValues[0].value;
-    else if(exchangeCurrency===currencyValues[1].currency)
-        return sumUAH/currencyValues[1].value;
-    else
-        return "ERROR! Wrong CURRENCY!"
-}
 let array_currency=[{currency:`USD`,value:44},{currency:`EUR`,value:53}];
 let sum = +prompt("Enter a Sum in UAH: ");
 let currency_exchange=prompt("Enter an exchange currency (USD or EUR): ");
+let exchange=(sum,array_currency,currency_exchange)=>(currency_exchange===array_currency[0].currency?sum/array_currency[0].value:(currency_exchange===array_currency[1].currency?sum/array_currency[1].value:"ERROR! Wrong CURRENCY!"));
+if(sum>=0)
 console.log(exchange(sum,array_currency,currency_exchange));
+else
+    console.log(`ERROR! Incorrect sum`);
