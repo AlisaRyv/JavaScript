72997e02c28ee2b237063ce51025681583d349ca
AlisaRyv <aliceryvkina2@gmail.com>
2026-05-29 20:08:17 +0300

    Finish Hw_5_10.html & Task_5_10.js

--- Homeworks/Homework_5/Homework_5_10/Task_5_10.js
+++ Homeworks/Homework_5/Homework_5_10/Task_5_10.js
@@ -1,3 +1,6 @@
-let arr=[`a`, 23, 90, true, `brak`, -35, false, 47, `b`, 5];
-let list=arr=>(document.write(`<ul>`),arr.map(item=>document.write(`<li>${item}</li>`)),   document.write(`</ul>`));
-list(arr);
+let arr=[-23, 13, 45, 0, -345, -2, -56, 87];
+let min=arr[0];
+let i=1;
+let check = (ar,int)=>(int<ar.length?(int++,list(ar,int)):(console.log(`END!`),console.log(`${min}`)));
+let list=(arr,i)=>(arr[i]<min?(min=arr[i],check(arr,i)):check(arr,i));
+list(arr,i);
