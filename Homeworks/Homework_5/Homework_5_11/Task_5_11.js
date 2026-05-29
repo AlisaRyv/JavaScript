6187a9bb0de4e627b74f18d87dac11bad1042c7c
AlisaRyv <aliceryvkina2@gmail.com>
2026-05-29 21:05:06 +0300

    Revert "Finish Hw_5_13.html & Task_5_13.js"
    This reverts commit 783a3ac735abeace78ab265d47d3e168cfa66ebf.

--- Homeworks/Homework_5/Homework_5_11/Task_5_11.js
+++ Homeworks/Homework_5/Homework_5_11/Task_5_11.js
@@ -2,5 +2,5 @@ let arr=[-13, 45, 0, -2, 87];
 let sum=0;
 let i=0;
 let check = (arr,i,sum)=>(i<arr.length-1?(i++,summator(arr,i,sum)):(console.log(`END!`),console.log(`${sum}`)));
-let summator=(arr,i,sum)=>(sum+=arr[i],check(arr,i,sum));
+let summator=(arr,i,sum)=>(sum+=arr[i],console.log(`${sum}`),check(arr,i,sum));
 summator(arr,i,sum);
