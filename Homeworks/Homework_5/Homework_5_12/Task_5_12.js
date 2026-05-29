e3a38453d551ccfa1e8c8ad415f7c428bde6abc9
AlisaRyv <aliceryvkina2@gmail.com>
2026-05-29 20:37:51 +0300

    Finish Hw_5_12.html & Task_5_12.js

--- Homeworks/Homework_5/Homework_5_12/Task_5_12.js
+++ Homeworks/Homework_5/Homework_5_12/Task_5_12.js
@@ -1,6 +1,12 @@
 let arr=[-13, 45, 0, -2, 87];
-let sum=0;
-let i=0;
-let check = (arr,i,sum)=>(i<arr.length-1?(i++,summator(arr,i,sum)):(console.log(`END!`),console.log(`${sum}`)));
-let summator=(arr,i,sum)=>(sum+=arr[i],console.log(`${sum}`),check(arr,i,sum));
-summator(arr,i,sum);
+let index1=+prompt("Enter a first index");
+let index2=+prompt("Enter a second index");
+let num=0;
+let swap = (arr, index1, index2) => (num = arr[index1], arr[index1] = arr[index2], arr[index2] = num);
+if((index1>=0&&index1<arr.length)&&(index2>=0&&index2<arr.length))
+{
+    swap(arr,index1,index2);
+    console.log(arr);
+}
+else
+    console.log(`ERROR!`)
