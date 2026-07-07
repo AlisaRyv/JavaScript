aa5a520bd9cc13159a8d8253a6cd3c3d514bdc1d
AlisaRyv <aliceryvkina2@gmail.com>
2026-07-07 18:21:38 +0300

    Finish les7_3.js & lesson7_3.html

--- Classworks/Lesson_7/Lesson_7_3/les7_3.js
+++ Classworks/Lesson_7/Lesson_7_3/les7_3.js
@@ -1,1 +1,35 @@
+// class User
+// {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//
+//     greeting()
+//     {
+//         return `Hello my name is ${this.name}!`;
+//     }
+// }
+//
+// class Customer extends User{
+//
+//     constructor(name, age, password) {
+//         super(name, age);
+//         this.password = password;
+//     }
+// }
+// let user1 = new User('petya', 123123);
+// console.log(user1, user1.greeting());
+function User(name, age) {
+    this.name = name;
+    this.age = age;
+}
 
+
+function Customer(name, age, password) {
+    User.apply(this, arguments);
+    this.password = password;
+}
+
+let customer = new Customer('vasya', 31, '1111');
+console.log(customer);
