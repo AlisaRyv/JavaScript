dfed01769abc77376e6b9917f48152c1256bfd4b
AlisaRyv <aliceryvkina2@gmail.com>
2026-07-10 17:44:36 +0300

    Start Hw_7_7.html & Task_7_7.js

---
+++ Homeworks/Homework_7/Homework_7_7/Task_7_7.js
@@ -0,0 +1,8 @@
+
+let auto=new Car(`X17`,`Range Rover`,2024, 200, 189);
+console.log(auto);
+auto.drive();
+auto.increaseMaxSpeed(90);
+auto.changeYear(1990);
+auto.addDriver({id:1,name:`Valera`,age:32});
+auto.info();
