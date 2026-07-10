d6c70ee19751ec6bc83e3f4fcdfb941e6cb272f3
AlisaRyv <aliceryvkina2@gmail.com>
2026-07-10 16:45:44 +0300

    Finish Hw_7_3.html & Task_7_3.js

--- Homeworks/Homework_7/Homework_7_2/Task_7_2.js
+++ Homeworks/Homework_7/Homework_7_2/Task_7_2.js
@@ -7,16 +7,16 @@ function User(id,name,surname,email,phone)
     this.phone=phone;
 }
 let array=[
-    new User(1,`Abba`, `Trial`, `abba@gmail.com`,12345),
-    new User(2,`Lary`, `Troy`, `lary@gmail.com`,123456),
+    new User(10,`Abba`, `Trial`, `abba@gmail.com`,12345),
+    new User(23,`Lary`, `Troy`, `lary@gmail.com`,123456),
     new User(3,`John`, `Krak`, `john@gmail.com`,1234567),
-    new User(4,`Alan`, `Lin`, `alan@gmail.com`,12345678),
+    new User(41,`Alan`, `Lin`, `alan@gmail.com`,12345678),
     new User(5,`Ray`, `Rean`, `ray@gmail.com`,123456789),
     new User(6,`Alex`, `Tulo`, `alex@gmail.com`,123450),
-    new User(7,`Alexandra`, `Wefi`, `alexandra@gmail.com`,1234501),
-    new User(8,`Gray`, `Hiker`, `gray@gmail.com`,12345123),
+    new User(72,`Alexandra`, `Wefi`, `alexandra@gmail.com`,1234501),
+    new User(18,`Gray`, `Hiker`, `gray@gmail.com`,12345123),
     new User(9,`Lui`, `Luy`, `lui@gmail.com`,12345012),
-    new User(10,`Mark`, `Quiet`, `mark@gmail.com`,12345345),
+    new User(100,`Mark`, `Quiet`, `mark@gmail.com`,12345345),
 ];
 console.log(array);
 let filteredArray=array.filter(item=>item.id%2===0);
