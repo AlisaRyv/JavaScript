cdd845f5371861b502fc493f20f5b9d1877c5e41
AlisaRyv <aliceryvkina2@gmail.com>
2026-07-10 16:31:27 +0300

    Start Hw_7_2.html & Task_7_2.js

---
+++ Homeworks/Homework_7/Homework_7_2/Task_7_2.js
@@ -0,0 +1,21 @@
+function User(id,name,surname,email,phone)
+{
+    this.id = id;
+    this.name=name;
+    this.surname=surname;
+    this.email=email;
+    this.phone=phone;
+}
+let array=[
+    new User(1,`Abba`, `Trial`, `abba@gmail.com`,12345),
+    new User(2,`Lary`, `Troy`, `lary@gmail.com`,123456),
+    new User(3,`John`, `Krak`, `john@gmail.com`,1234567),
+    new User(4,`Alan`, `Lin`, `alan@gmail.com`,12345678),
+    new User(5,`Ray`, `Rean`, `ray@gmail.com`,123456789),
+    new User(6,`Alex`, `Tulo`, `alex@gmail.com`,123450),
+    new User(7,`Alexandra`, `Wefi`, `alexandra@gmail.com`,1234501),
+    new User(8,`Gray`, `Hiker`, `gray@gmail.com`,12345123),
+    new User(9,`Lui`, `Luy`, `lui@gmail.com`,12345012),
+    new User(10,`Mark`, `Quiet`, `mark@gmail.com`,12345345),
+]
+console.log(array);
