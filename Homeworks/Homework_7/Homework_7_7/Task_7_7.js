1292a3c8295db789613ea7aaea1ee87575c87f79
AlisaRyv <aliceryvkina2@gmail.com>
2026-07-10 17:49:56 +0300

    Finish Hw_7_7.html & Task_7_7.js

--- Homeworks/Homework_7/Homework_7_7/Task_7_7.js
+++ Homeworks/Homework_7/Homework_7_7/Task_7_7.js
@@ -1,4 +1,41 @@
+class Car
+{
+    constructor(mode,producer,year,maxSpeed,volumeEngine)
+    {
+        this.mode = mode;
+        this.producer = producer;
+        this.year = year;
+        this.maxSpeed = maxSpeed;
+        this.volumeEngine = volumeEngine;
+    };
+    drive()
+    {
+        console.log(`Їдемо зі швидкістю ${this.maxSpeed} на годину`);
+    };
+    info()
+    {
+        for (const key in this)
+        {
+            console.log(key,` - `,this[key]);
+        }
+    };
+    increaseMaxSpeed(newSpeed)
+    {
+    if(newSpeed > 0)
+        this.maxSpeed += newSpeed;
+    };
 
+    changeYear(newValue) {
+        if (newValue > 1815)
+            this.year = newValue;
+    };
+
+    addDriver(driver) {
+        if (driver)
+            this.driver = driver;
+    };
+
+}
 let auto=new Car(`X17`,`Range Rover`,2024, 200, 189);
 console.log(auto);
 auto.drive();
