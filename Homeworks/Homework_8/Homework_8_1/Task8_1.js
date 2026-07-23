5f878e0869602b26154eebe8479bbe08b5dccbd2
AlisaRyv <aliceryvkina2@gmail.com>
2026-07-23 11:34:02 +0300

    Finish Hw8_1.html & Task8_1.js

--- Homeworks/Homework_8/Homework_8_1/Task8_1.js
+++ Homeworks/Homework_8/Homework_8_1/Task8_1.js
@@ -1,3 +1,18 @@
+let coursesAndDurationArray = [
 
+    {title: `JavaScript Complex`, monthDuration: 5},
 
+    {title: `Java Complex`, monthDuration: 6},
 
+    {title: `Python Complex`, monthDuration: 6},
+
+    {title: `QA Complex`, monthDuration: 4},
+
+    {title: `FullStack`, monthDuration: 7},
+
+    {title: `Frontend`, monthDuration: 4}
+
+];
+
+let newCourses = coursesAndDurationArray.map((course,index) => ({...course,id: index+1}));
+console.log(newCourses);
