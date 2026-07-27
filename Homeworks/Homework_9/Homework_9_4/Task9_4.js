50392a61346d7cd78762f407805ac5b5e092556f
AlisaRyv <aliceryvkina2@gmail.com>
2026-07-27 21:33:24 +0300

    Finish Hw9_4.html & Task9_4.js

--- Homeworks/Homework_9/Homework_9_4/Task9_4.js
+++ Homeworks/Homework_9/Homework_9_4/Task9_4.js
@@ -2,15 +2,15 @@ let coursesAndDurationArray = [
 
     {title: `JavaScript Complex`, monthDuration: 5},
 
-{title: `Java Complex`, monthDuration: 6},
+    {title: `Java Complex`, monthDuration: 6},
 
-{title: `Python Complex`, monthDuration: 6},
+    {title: `Python Complex`, monthDuration: 6},
 
-{title: `QA Complex`, monthDuration: 4},
+    {title: `QA Complex`, monthDuration: 4},
 
-{title: `FullStack`, monthDuration: 7},
+    {title: `FullStack`, monthDuration: 7},
 
-{title: `Frontend`, monthDuration: 4}
+    {title: `Frontend`, monthDuration: 4}
 
 ];
 
@@ -18,7 +18,17 @@ let mainDiv=document.createElement(`divMain`);
 for (let course of coursesAndDurationArray)
 {
     let div = document.createElement('div');
-    div.innerText = `${course.title} ${course.monthDuration}`;
+    div.classList.add(`item`);
+
+    let h1 = document.createElement('h1');
+    h1.classList.add(`heading`);
+    h1.innerText = course.title;
+
+    let p = document.createElement('p');
+    p.classList.add(`description`);
+    p.innerText = course.monthDuration;
+
+    div.append(h1,p);
     mainDiv.appendChild(div);
 }
 document.body.appendChild(mainDiv);
